7efa2d970787cd09b4c5624c6421be31ecc4e921
Hafsteinn Ragnarsson <haffi921@gmail.com>
2022-02-23 18:50:13 +0100

    batch_init also messages the PID now

--- src/batch_init.ts
+++ src/batch_init.ts
@@ -9,35 +9,35 @@ function CopyJsonInputToBatch() {
     const dataFields = Object.keys(data);
 
     function initBatchData(key, value) {
-        if(!jatos.batchSession.defined("/" + key)) {
+        if (!jatos.batchSession.defined("/" + key)) {
             return jatos.batchSession.set(key, value);
         }
         return Promise.resolve();
     }
 
     function CopyKeyValue(index) {
-        if(index < dataFields.length) {
+        if (index < dataFields.length) {
             const key = dataFields[index];
             initBatchData(key, data[key])
-            .then(function() {
-                error_counter = 0;
-                CopyKeyValue(index + 1);
-            })
-            .catch(function(e) {
-                error_counter += 1;
-                if (error_counter > 2) {
-                    console.error(e);
-                    return;
-                }
-                CopyKeyValue(index);
-            });
+                .then(function () {
+                    error_counter = 0;
+                    CopyKeyValue(index + 1);
+                })
+                .catch(function (e) {
+                    error_counter += 1;
+                    if (error_counter > 2) {
+                        console.error(e);
+                        return;
+                    }
+                    CopyKeyValue(index);
+                });
         }
     }
 
     CopyKeyValue(0);
 }
 
-jatos.onLoad(function() {
+jatos.onLoad(function () {
     CopyJsonInputToBatch();
-    jatos.startNextComponent();
+    jatos.startNextComponent(null, jatos.urlQueryParameters.PROLIFIC_PID.substring(0, 6));
 });
